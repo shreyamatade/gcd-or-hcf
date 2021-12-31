8ca2eb6568c6bc0c38165b12f0973db944f19132
shreyamatade <85739955+shreyamatade@users.noreply.github.com>
2021-12-31 12:21:00 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //gcd or hcf
+    int a;
+    cin>>a;
+
+    int b;
+    cin>>b;
+
+    cout<<__gcd(a,b)<<endl;
+
+
+
+}
